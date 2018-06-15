553a374cec2a470ac6aaff21a93f80361cbf6c9e
Gaurav Karvir <genroks@gmail.com>
2018-06-15 16:36:32 +0100

    added support for supertest

--- e2e/app.test.js
+++ e2e/app.test.js
@@ -1,19 +1,44 @@
-describe('Example', () => {
-  beforeEach(async () => {
-    await device.reloadReactNative();
-  });
-  
-  it('should have welcome screen', async () => {
-    await expect(element(by.id('welcome'))).toBeVisible();
-  });
-  
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
-  });
-  
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
-  });
+const request = require('supertest');
+
+const host_url = 'https://jsonplaceholder.typicode.com';
+
+
+describe('Detox ReactNative Test with Jest and SuperTest', () => {
+    beforeEach(async () => {
+        await device.reloadReactNative();
+    });
+
+    it('should be able to query an API', (done) => {
+        const container = request(host_url + '/posts/1');
+
+        container.get('')
+            .expect(200, done);
+    });
+
+    it('should be able to submit to an API', (done) => {
+        const container = request(host_url + '/posts');
+
+        container.post('')
+            .send({title: 'animal', body: 'cat', userId: 1})
+            .set('Accept', 'application/json')
+            .end((err, res) => {
+                console.log('response', JSON.stringify(res.body));
+            });
+
+    });
+
+
+    it('should have welcome screen', async () => {
+        await expect(element(by.id('welcome'))).toBeVisible();
+    });
+
+    it('should show hello screen after tap', async () => {
+        await element(by.id('hello_button')).tap();
+        await expect(element(by.text('Hello!!!'))).toBeVisible();
+    });
+
+    it('should show world screen after tap', async () => {
+        await element(by.id('world_button')).tap();
+        await expect(element(by.text('World!!!'))).toBeVisible();
+    });
 });
